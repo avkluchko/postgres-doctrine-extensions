afaf2baea3c3f942e1eb0b2f7abf7230690aff8a
Aleksandr Kluchko <avkluchko@yandex.ru>
2019-10-06 18:06:22 +0500

    Setup Tests

---
+++ tests/DQL/DatePartTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DQL;
+
+use PHPUnit\Framework\TestCase;
+
+class DatePartTest extends TestCase
+{
+    public function testInit()
+    {
+        $this->assertEquals(23, 23);
+    }
+}
